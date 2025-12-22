4610d4a4802c43d6a1e98c4f5e158671e40a8049
mujtabashaikh025 <mujtabashaikh025@gmail.com>
2025-12-22 09:07:55 +0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-app_page = ste.Page(page="app.py")
+app_page = st.Page(page="app.py")
 compliance_page = st.Page(page="pages/compliance.py")
 pg=st.navigation(pages=[compliance_page])
-pg=st.navigation(pages=[app_page])
+#pg=st.navigation(pages=[app_page])
 pg.run()
