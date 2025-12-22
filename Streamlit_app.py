a37e7f1d4d183dce8ee66ad08899932cedd1c049
mujtabashaikh025 <mujtabashaikh025@gmail.com>
2025-12-22 09:02:07 +0400

    Create Streamlit_app.py

---
+++ Streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+app_page = srt.Page(page="app.py")
+compliance_page = st.Page(page="pages/compliance.py")
+pg=st.navigation(pages=[compliance_page])
+pg=st.navigation(pages=[app_page])
+pg.run()
